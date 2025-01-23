3469e54ec99e16bd32dbdc9a46d7b221a2bcc6da
AyoDano <100164168+AyoDano@users.noreply.github.com>
2025-01-23 12:24:19 +0100

    Small Changes in code

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,7 +6,6 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Hola")),
       body: Center(
         child: Text("Hola Snacky"),
       ),
